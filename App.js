7f5aa4e14689c32b3a96e6acc941f45c33637868
Daniel Jussef <jussef.daniel@gmail.com>
2024-12-12 11:44:35 -0600

    init

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { getLatestGames } from "./lib/metacritic";
+import icon from "./assets/icon.png";
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => setGames(games));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={icon} style={{ width: 200, height: 200 }} />
       <StatusBar style="auto" />
+      <Text>Aplicación de React Native</Text>
+      <Button title="Presionar" onPress={() => alert("Hola Mundo")} />
+      {games.map((game) => (
+        <View key={game.slug} style={styles.card}>
+          <Image source={{ uri: game.image }} style={{ width: 100, height: 100 }} />
+        </View>
+      ))}
+      <Text>Fin de la aplicación</Text>
     </View>
   );
 }
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff898",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
